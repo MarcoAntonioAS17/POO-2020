a9b294e5a2fac2871e2aeceff9aa27bdc9c0e100
Gonzalo Antonio Sosa Malaga <gonzasosa@outlook.com>
2020-04-09 13:08:17 -0500

    Interfaces basics added

---
+++ Interfaces/InterfacesBasics/src/main/java/mx/uv/fiee/iinf/poo/Main.java
@@ -0,0 +1,15 @@
+package mx.uv.fiee.iinf.poo;
+
+public class Main {
+
+    public static void main (String [] args) {
+        Circle c = new Circle (5, 5, 10);
+        c.paint ();
+    }
+
+}
+
+
+
+
+
