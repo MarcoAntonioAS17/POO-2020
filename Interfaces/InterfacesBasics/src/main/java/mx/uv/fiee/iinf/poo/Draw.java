6e5baf9bb721d12bd7854bf91f1f2632dfa3aded
Gonzalo Antonio Sosa Malaga <gonzasosa@outlook.com>
2020-04-09 13:07:26 -0500

    Wrong project removed

--- Interfaces/InterfacesBasics/src/main/java/mx/uv/fiee/iinf/poo/Draw.java
+++
@@ -1,6 +0,0 @@
-package mx.uv.fiee.iinf.poo;
-
-interface Draw {
-    void paint ();
-    void fill ();
-}
--- Interfaces/InterfacesBasics/src/main/java/mx/uv/fiee/iinf/poo/Shape.java
+++
@@ -1,5 +0,0 @@
-package mx.uv.fiee.iinf.poo;
-
-public interface Shape {
-    void calculateArea ();
-}
